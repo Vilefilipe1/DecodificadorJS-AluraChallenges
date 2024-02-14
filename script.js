5c3fd37da63d1c630584aa68edb7751de126b51d
Filipe C <30263686+Vilefilipe1@users.noreply.github.com>
2024-02-14 13:22:31 -0300

    Feito o sistema de cript e descript

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
 let video = document.querySelector(".loading-video");
 let loading = document.getElementsByClassName("loading")[0];
 let main = document.querySelector("main");
-let input = document.getElementById("input").value;
+let input = document.getElementById("input");
 let titulo = document.getElementById("msg-titulo");
 let principal = document.getElementById("msg-principal");
-let palavraCripto = "";
+let palavraSeparada = [];
+let palavraSecreta = [];
+let novaPalavra = "";
 
-function cripto(letra, frase) {
-    if (input.includes(letra)) {
-        palavraCripto = palavraCripto + frase;
-
+function ifvalue(value, letra, palavra, index, array, length) {
+    if (length != false) {
+        value == letra ? array.splice(index, length, letra):""
+    } else if (length == false) {
+        value == letra ? value = palavra:""
+        return value
     }
 }
 
-function descriptografar() {
-    titulo.innerHTML = "";
-    principal.innerHTML = "aaaaaaaaaaaaa";
+
+function trocar(value, index, array) {
+    value = ifvalue(value, "a", "ai", index, array, false)
+    value = ifvalue(value, "e", "enter", index, array, false)
+    value = ifvalue(value, "i", "imes", index, array, false)
+    value = ifvalue(value, "o", "ober", index, array, false)
+    value = ifvalue(value, "u", "ufat", index, array, false)
+
+    palavraSecreta.push(value)
+}
+
+function trocarRev(value, index, array) {
+    ifvalue(value, "a", "ai", index, array, 2)
+    ifvalue(value, "e", "enter", index, array, 5)
+    ifvalue(value, "i", "imes", index, array, 4)
+    ifvalue(value, "o", "ober", index, array, 4)
+    ifvalue(value, "u", "ufat", index, array, 4)
+
+    return array
 }
 
 function criptografar() {
+    palavraSeparada = input.value.split("");
+    palavraSeparada.forEach(trocar);
+    let palavraCript = palavraSecreta.join("");
     titulo.innerHTML = "";
-    palavraCripto = "";
-    
-    cripto("a", "ai")
-    cripto("e", "enter")
-    cripto("i", "imes")
-    cripto("o", "ober")
-    cripto("u", "ufat")
+    principal.innerHTML = palavraCript;
 
-    principal.innerHTML = palavraCripto;
+    palavraSecreta = [];
 
+}
+
+function descriptografar() {
+    palavraSeparada = input.value.split("");
+    palavraSeparada.forEach(trocarRev);
+    let palavraCript = palavraSeparada.join("");
+    titulo.innerHTML = "";
+    principal.innerHTML = palavraCript;
+
+    palavraSeparada = [];
 }
 
 video.addEventListener("ended", (event) => {
